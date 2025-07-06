a7c251e87e8bd4ed8a73cd8b068a484bee26ab14
HARRYEZAD <hamzafayaz002@gmail.com>
2025-07-07 02:39:25 +0500

    Resol

--- apps/docs/src/app/Components/Welcome-page/page.tsx
+++ apps/docs/src/app/Components/Welcome-page/page.tsx
@@ -1,3 +1,8 @@
 export default function WelcomePage() {
-  return <div>...</div>
+  return (
+    <div>
+      <h1>Welcome Page</h1>
+      <p>This is a placeholder component.</p>
+    </div>
+  );
 }
